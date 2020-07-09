5aa103cbbe0048cc3295d9ea242046e73f17cb58
Batool Abbas <abbassbatoolali@gmail.com>
2020-07-09 17:56:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello batool abbas
+      hello batool abbas ali
     </div>
   );
 }
